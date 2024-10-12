c592ee80a4db5e3a6327fed6eae2d1678b9fa66e
Engin Karakurt <155079545+engin-karakurt@users.noreply.github.com>
2024-10-12 16:46:14 +0200

    Bump version

--- src/main/java/de/enginkarakurt/microbotlauncherunix/MicrobotLauncherUnix.java
+++ src/main/java/de/enginkarakurt/microbotlauncherunix/MicrobotLauncherUnix.java
@@ -3,7 +3,7 @@ package de.enginkarakurt.microbotlauncherunix;
 import de.enginkarakurt.microbotlauncherunix.util.FileUtil;
 
 public class MicrobotLauncherUnix {
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
 
     public static void main(String[] args) {
        runMicrobotLauncher();
